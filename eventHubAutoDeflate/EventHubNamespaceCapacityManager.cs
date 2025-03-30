2bcf34d1519854d94bd4540b7d02d4c5443a3357
Erwin Kramer <r3verse@gmail.com>
2025-03-30 11:38:04 +0200

    refactor into EventHubNamespaceCapacityManager

---
+++ eventHubAutoDeflate/EventHubNamespaceCapacityManager.cs
@@ -0,0 +1,106 @@
+using Azure.Core;
+using Azure.Monitor.Query;
+using Azure.ResourceManager;
+using Azure.ResourceManager.EventHubs;
+using Azure.ResourceManager.EventHubs.Models;
+
+public class EventHubNamespaceCapacityManager
+{
+    private readonly string _namespaceResourceId;
+    private readonly int _minimumCapacity;
+    private readonly ArmClient _armClient;
+    private readonly MetricsQueryClient _metricsQueryClient;
+
+    public EventHubNamespaceCapacityManager(string namespaceResourceId, int minimumCapacity, TokenCredential credential)
+    {
+        _namespaceResourceId = namespaceResourceId;
+        _minimumCapacity = minimumCapacity;
+        _armClient = new ArmClient(credential);
+        _metricsQueryClient = new MetricsQueryClient(credential);
+    }
+
+    public async Task DeflateNamespaceIfNeeded()
+    {
+        var namespaceIdentifier = new ResourceIdentifier(_namespaceResourceId);
+        var eventHubsNamespace = _armClient.GetEventHubsNamespaceResource(namespaceIdentifier);
+        EventHubsNamespaceData namespaceData;
+
+        try
+        {
+            namespaceData = (await eventHubsNamespace.GetAsync()).Value.Data;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Could not get namespace data, with message: {ex.Message}");
+            throw;
+        }
+
+        if (!IsAutoInflateEnabled(namespaceData))
+        {
+            Console.WriteLine("Namespace is not set to auto inflate, skip deflate process");
+            return;
+        }
+
+        if (await IsNamespaceThrottled(namespaceIdentifier!))
+        {
+            Console.WriteLine("Namespace is throttled, skip deflate process");
+            return;
+        }
+
+        if (!await TryDeflateNamespace(eventHubsNamespace, namespaceData))
+        {
+            Console.WriteLine($"Namespace capacity ({namespaceData.Sku.Capacity}) is already at minimum, or same as current capacity, skip deflate process");
+            return;
+        }
+    }
+
+    private async Task<bool> IsNamespaceThrottled(string namespaceIdentifier)
+    {
+        var metricResult = (await _metricsQueryClient.QueryResourceAsync(
+            namespaceIdentifier,
+            new[] { "ThrottledRequests" },
+            new MetricsQueryOptions
+            {
+                Granularity = TimeSpan.FromHours(1),
+                TimeRange = new QueryTimeRange(DateTime.UtcNow.AddHours(-1), DateTime.UtcNow)
+            }
+        )).Value.Metrics.FirstOrDefault();
+
+        if (metricResult == null)
+        {
+            throw new InvalidOperationException("ThrottledRequests metric not found");
+        }
+
+        var throttledRequestTotal = metricResult.TimeSeries.FirstOrDefault()?.Values.FirstOrDefault()?.Total ?? 0;
+        Console.WriteLine($"Namespace has {throttledRequestTotal} throttled requests in the last hour.");
+
+        return throttledRequestTotal > 0;
+    }
+
+    private bool IsAutoInflateEnabled(EventHubsNamespaceData namespaceData)
+    {
+        return namespaceData.IsAutoInflateEnabled.HasValue && namespaceData.IsAutoInflateEnabled.Value;
+    }
+
+    private async Task<bool> TryDeflateNamespace(EventHubsNamespaceResource eventHubsNamespace, EventHubsNamespaceData namespaceData)
+    {
+        var newCapacity = namespaceData.Sku.Capacity - 1;
+
+        if (newCapacity < _minimumCapacity || newCapacity == namespaceData.Sku.Capacity)
+        {
+            return false;
+        }
+
+        var namespaceDataRequest = new EventHubsNamespaceData(namespaceData.Location)
+        {
+            Sku = new EventHubsSku(namespaceData.Sku.Name)
+            {
+                Capacity = newCapacity
+            }
+        };
+
+        var namespaceDataUpdated = (await eventHubsNamespace.UpdateAsync(namespaceDataRequest)).Value.Data;
+        Console.WriteLine($"New capacity set to: {namespaceDataUpdated.Sku.Capacity}, previous capacity was {namespaceData.Sku.Capacity}");
+        return true;
+    }
+}
--- eventHubAutoDeflate/Program.cs
+++ eventHubAutoDeflate/Program.cs
@@ -5,101 +5,29 @@ Auto-deflate Event Hubs namespace capacity based on a minimum capacity threshold
 */
 
 using Azure.Identity;
-using Azure.Monitor.Query;
-using Azure.ResourceManager;
-using Azure.ResourceManager.EventHubs;
-using Azure.ResourceManager.EventHubs.Models;
 
-var subscriptionId = "beb880cc-af9a-4e4d-8e8e-54739967674f";
-var resourceGroupName = "rg-events";
-var namespaceName = "fref23v32v32f32f";
+// comma separated resource ids of the Event Hubs namespace
+var namespaceResourceIds = "/subscriptions/beb880cc-af9a-4e4d-8e8e-54739967674f/resourceGroups/rg-events/providers/Microsoft.EventHub/namespaces/g5465jtrfdgdfg3443g34,/subscriptions/beb880cc-af9a-4e4d-8e8e-54739967674f/resourceGroups/rg-ade-sandbox/providers/Microsoft.EventHub/namespaces/asdads2d2d21d";
 var namespaceInstanceMinimumCapacity = 1; // should be set to a realistic value depending on your lower bound workload
 
 var credential = new DefaultAzureCredential();
-var armClient = new ArmClient(credential);
-var metricsQueryClient = new MetricsQueryClient(credential);
 
-var namespaceIdentifier = EventHubsNamespaceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName);
+var exceptions = new List<Exception>();
 
-var eventHubsNamespace = armClient.GetEventHubsNamespaceResource(namespaceIdentifier);
-EventHubsNamespaceData namespaceData;
-
-try
+foreach (var namespaceResourceId in namespaceResourceIds.Split(','))
 {
-    namespaceData = (await eventHubsNamespace.GetAsync()).Value.Data;
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Could not get namespace data, with message: {ex.Message}");
-    throw;
-}
-
-if (!IsAutoInflateEnabled(namespaceData))
-{
-    Console.WriteLine("Namespace is not set to auto inflate, skip deflate process");
-    return;
-}
-
-if (await IsNamespaceThrottled(metricsQueryClient, namespaceIdentifier!))
-{
-    Console.WriteLine("Namespace is throttled, skip deflate process");
-    return;
-}
-
-if (!await TryDeflateNamespace(eventHubsNamespace, namespaceData, namespaceInstanceMinimumCapacity))
-{
-    Console.WriteLine($"Namespace capacity ({namespaceData.Sku.Capacity}) is already at minimum, or same as current capacity, skip deflate process");
-    return;
-}
-
-/// <summary>
-/// Queries the Event Hub namespace for the ThrottledRequests metric to determine if the namespace is throttled.
-/// Uses a time span of an hour to query the metric.
-/// </summary>
-async Task<bool> IsNamespaceThrottled(MetricsQueryClient metricsQueryClient, string namespaceIdentifier)
-{
-    var metricResult = (await metricsQueryClient.QueryResourceAsync(
-        namespaceIdentifier,
-        ["ThrottledRequests"],
-        new MetricsQueryOptions()
-        {
-            Granularity = new TimeSpan(1, 0, 0),
-            TimeRange = new QueryTimeRange(DateTime.UtcNow.AddHours(-1), DateTime.UtcNow)
-        }
-    )).Value.Metrics.FirstOrDefault();
-
-    if (metricResult == null)
+    try
     {
-        throw new InvalidOperationException("ThrottledRequests metric not found");
+        var eventHubNamespaceCapacityManager = new EventHubNamespaceCapacityManager(namespaceResourceId, namespaceInstanceMinimumCapacity, credential);
+        await eventHubNamespaceCapacityManager.DeflateNamespaceIfNeeded();
     }
-
-    var throttledRequestTotal = metricResult.TimeSeries.FirstOrDefault()!.Values.FirstOrDefault()!.Total;
-    return throttledRequestTotal > 0;
-}
-
-bool IsAutoInflateEnabled(EventHubsNamespaceData namespaceData)
-{
-    return namespaceData.IsAutoInflateEnabled.HasValue && namespaceData.IsAutoInflateEnabled.Value;
-}
-
-async Task<bool> TryDeflateNamespace(EventHubsNamespaceResource eventHubsNamespace, EventHubsNamespaceData namespaceData, int minimumCapacity)
-{
-    var newCapacity = namespaceData.Sku.Capacity - 1;
-
-    if (newCapacity < minimumCapacity || newCapacity == namespaceData.Sku.Capacity)
+    catch (Exception ex)
     {
-        return false;
+        exceptions.Add(new Exception($"Error processing namespace {namespaceResourceId}: {ex.Message}", ex));
     }
+}
 
-    EventHubsNamespaceData namespaceDataRequest = new EventHubsNamespaceData(namespaceData.Location)
-    {
-        Sku = new EventHubsSku(namespaceData.Sku.Name)
-        {
-            Capacity = newCapacity
-        }
-    };
-
-    var namespaceDataUpdated = (await eventHubsNamespace.UpdateAsync(namespaceDataRequest)).Value.Data;
-    Console.WriteLine($"New capacity set to: {namespaceDataUpdated.Sku.Capacity}, previous capacity was {namespaceData.Sku.Capacity}");
-    return true;
+if (exceptions.Any())
+{
+    throw new AggregateException("One or more namespaces failed to deflate.", exceptions);
 }
