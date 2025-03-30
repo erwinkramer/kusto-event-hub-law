a6f8614e0646234a061c220bf94f5418a398e29a
Erwin Kramer <r3verse@gmail.com>
2025-03-30 15:53:33 +0200

    add EventHubNamespaceConfig

---
+++ eventHubAutoDeflate/EventHubNamespaceConfig.cs
@@ -0,0 +1,12 @@
+internal class EventHubNamespaceConfig
+{
+    /// <summary>
+    /// Valid resource ID of the Event Hubs namespace
+    /// </summary>
+    public required string ResourceId { get; set; }
+
+    /// <summary>
+    /// Should be set to a realistic value depending on your lower bound workload
+    /// </summary>
+    public required int MinimumCapacity { get; set; }
+}
--- eventHubAutoDeflate/Program.cs
+++ eventHubAutoDeflate/Program.cs
@@ -6,24 +6,34 @@ Auto-deflate Event Hubs namespace capacity based on a minimum capacity threshold
 
 using Azure.Identity;
 
-// comma separated resource ids of the Event Hubs namespace
-var namespaceResourceIds = "/subscriptions/beb880cc-af9a-4e4d-8e8e-54739967674f/resourceGroups/rg-events/providers/Microsoft.EventHub/namespaces/g5465jtrfdgdfg3443g34,/subscriptions/beb880cc-af9a-4e4d-8e8e-54739967674f/resourceGroups/rg-ade-sandbox/providers/Microsoft.EventHub/namespaces/asdads2d2d21d";
-var namespaceInstanceMinimumCapacity = 1; // should be set to a realistic value depending on your lower bound workload
+var eventHubNamespaceConfigs = new List<EventHubNamespaceConfig>
+{
+    new EventHubNamespaceConfig
+    {
+        ResourceId = "/subscriptions/beb880cc-af9a-4e4d-8e8e-54739967674f/resourceGroups/rg-events/providers/Microsoft.EventHub/namespaces/g5465jtrfdgdfg3443g34",
+        MinimumCapacity = 1
+    },
+    new EventHubNamespaceConfig
+    {
+        ResourceId = "/subscriptions/beb880cc-af9a-4e4d-8e8e-54739967674f/resourceGroups/rg-ade-sandbox/providers/Microsoft.EventHub/namespaces/asdads2d2d21d",
+        MinimumCapacity = 1 
+    }
+};
 
 var credential = new DefaultAzureCredential();
 
 var exceptions = new List<Exception>();
 
-foreach (var namespaceResourceId in namespaceResourceIds.Split(','))
+foreach (var eventHubNamespaceConfig in eventHubNamespaceConfigs)
 {
     try
     {
-        var eventHubNamespaceCapacityManager = new EventHubNamespaceCapacityManager(namespaceResourceId, namespaceInstanceMinimumCapacity, credential);
+        var eventHubNamespaceCapacityManager = new EventHubNamespaceCapacityManager(eventHubNamespaceConfig.ResourceId, eventHubNamespaceConfig.MinimumCapacity, credential);
         await eventHubNamespaceCapacityManager.DeflateNamespaceIfNeeded();
     }
     catch (Exception ex)
     {
-        exceptions.Add(new Exception($"Error processing namespace {namespaceResourceId}: {ex.Message}", ex));
+        exceptions.Add(new Exception($"Error processing namespace {eventHubNamespaceConfig.ResourceId}: {ex.Message}", ex));
     }
 }
 
